f7bc7681a5d3c93544bf14a424897eb003be3c4f
Manoj Zaf <128605103+TheZaf@users.noreply.github.com>
2026-08-13 11:50:12 +0530

    Updated a typo in footer

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer className="flex flex-col md:flex-row gap-3 items-center justify-around w-full py-4 text-sm bg-black text-white/70">
-      <p>Copyright © 2026 Zaflix ⚡. A Build by Zaf.</p>
+      <p>Copyright © 2026 CryptoLens ⚡. A Build by Zaf.</p>
       <div className="flex items-center gap-4">
         <a href="https://github.com/TheZaf" className="hover:text-white transition-all">Contact Zaf</a>
         <div className="h-8 w-px bg-white/20"></div>
